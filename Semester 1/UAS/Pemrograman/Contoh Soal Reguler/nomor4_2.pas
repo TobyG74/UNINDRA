8adaad7d747713ee4404eca35d67f0f7727d7e93
Tobi Saputra <tobisaputra11@gmail.com>
2023-01-06 21:09:23 +0700

    Tambahan

--- Semester 1/UAS/Pemrograman/Contoh Soal Reguler/nomor4_2.pas
+++ Semester 1/UAS/Pemrograman/Contoh Soal Reguler/nomor4_2.pas
@@ -1,13 +1,16 @@
 program nomor4;
 uses crt;
 
-var a, b: integer;
+var a, b, baris, kolom: integer;
 
 begin
     clrscr;
-    for a := 1 to 3 do
+    write('Input jumlah baris : '); readln(baris);
+    write('Input jumlah kolom : '); readln(kolom);
+    writeln('==============================');
+    for a := 1 to baris do
     begin
-        for b := 1 to 5 do
+        for b := 1 to kolom do
         begin
             if (a mod 2 = 0) and (b mod 2 = 0) then write('X ')
             else if (a mod 2 = 1) and (b mod 2 = 1) then write('X ')
