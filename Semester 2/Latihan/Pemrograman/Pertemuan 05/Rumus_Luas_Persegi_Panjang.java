717682d341375e2f29dbc5c0fea15eb479db89e7
Tobi Saputra <32604979+TobyG74@users.noreply.github.com>
2023-04-05 13:28:01 +0700

    Create Rumus_Luas_Persegi_Panjang.java

---
+++ Semester 2/Latihan/Pemrograman/Pertemuan 05/Rumus_Luas_Persegi_Panjang.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Rumus_Luas_Persegi_Panjang {
+    public static void main (String[] args) {
+        Scanner in = new Scanner(System.in);
+   
+        // NILAI
+        int panjang = 8;
+        int lebar = 5;
+       
+        // OUTPUT
+        int hasil = panjang * lebar;
+        System.out.println("Luas Persegi Panjang : " + hasil);
+    }
+}
