d28dc6247412685e975387bd7e1fa2aa51ce57c1
Tobi Saputra <32604979+TobyG74@users.noreply.github.com>
2023-04-05 13:31:03 +0700

    Create Rumus_Luas_Persegi.java

---
+++ Semester 2/Latihan/Pemrograman/Pertemuan 5/Rumus_Luas_Persegi.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Rumus_Luas_Persegi {
+    public static void main (String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        // NILAI
+        int sisi = 5;
+       
+        // OUTPUT
+        int hasil = sisi * sisi;
+        System.out.println("Luas Persegi : " + hasil);
+
+    }
+}
