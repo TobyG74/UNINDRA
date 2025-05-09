646a71ccf89173a281256ae4c80554f74e7d495b
Tobi Saputra <tobisaputra11@gmail.com>
2025-05-09 19:55:12 +0700

    feat: update

--- Semester 6/UTS/Pemrograman Visual/README.md
+++ Semester 6/UTS/Pemrograman Visual/README.md
@@ -12,3 +12,35 @@
 -   Pilih File > Open Project
 -   Pilih Folder `uts_tobi` yang sudah dipindahkan ke Folder `NetBeansProjects`
 -   Klik `Open Project`
+
+## Setting Database
+
+![Database](./database.png)
+
+### Buat Database Auto
+
+-   Buka `XAMPP` dan Jalankan `Apache` dan `MySQL`
+-   Buka `phpMyAdmin` di Browser (biasanya di `http://localhost/phpmyadmin`)
+-   Klik `Import` di phpMyAdmin
+-   Pilih File `camping.sql` yang ada di Folder
+
+## Buat Database Manual
+
+-   Buka `XAMPP` dan Jalankan `Apache` dan `MySQL`
+-   Buka `phpMyAdmin` di Browser (biasanya di `http://localhost/phpmyadmin`)
+-   Klik `New`
+-   Buat Database dengan nama `camping`
+-   Klik `SQL` dan Copy Paste Query di bawah ini
+
+```sql
+CREATE TABLE `camping` (
+  `kode_alat` varchar(4) NOT NULL,
+  `nama_alat` varchar(50) NOT NULL,
+  `merk` varchar(50) NOT NULL,
+  `kondisi` varchar(50) NOT NULL,
+  `kelengkapan` TEXT NOT NULL,
+  `harga_sewa` INT NOT NULL,
+  `stok` INT NOT NULL,
+  PRIMARY KEY (`kode_alat`),
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+```
---
+++ Semester 6/UTS/Pemrograman Visual/camping.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 09, 2025 at 02:50 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `camping`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `alat_camping`
+--
+
+CREATE TABLE `alat_camping` (
+  `kode_alat` varchar(4) NOT NULL,
+  `nama_alat` varchar(50) NOT NULL,
+  `merk` varchar(50) NOT NULL,
+  `kondisi` varchar(50) NOT NULL,
+  `kelengkapan` text NOT NULL,
+  `harga_sewa` int(11) NOT NULL,
+  `stok` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `alat_camping`
+--
+ALTER TABLE `alat_camping`
+  ADD PRIMARY KEY (`kode_alat`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
